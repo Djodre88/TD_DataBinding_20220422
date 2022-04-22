07b062ace9523e852a3517916f17be3b37630142
djodre <jodre.diangienda@gmail.com>
2022-04-22 14:32:09 +0200

    Fin du projet + consignes nouveau projet

--- app/src/main/java/com/example/td_databinding_20220422/MainActivity.kt
+++ app/src/main/java/com/example/td_databinding_20220422/MainActivity.kt
@@ -8,6 +8,17 @@ import androidx.databinding.ObservableInt
 import androidx.lifecycle.ViewModelProvider
 import com.example.td_databinding_20220422.databinding.ActivityMainBinding
 
+// => 1er challenge : Two ways data binding. Regarder la doc
+// OBJECTIF :   ajouter un slider qui permet de modifier la valeur de l'âge
+//              et va mettre à jour l'observable ou le ViewModel
+//              et donc va afficher la valeur de l'âge modifié
+
+// => 2e challenge : pour la correction d'hier, jeudi 21 avril (deux dés) : le refaire avec un ViewModelScope
+// OBJECTIF :   1- Ajouter la dépendance pour le ViewModelScope
+//              2- Dans le ViewModel, créer un MutableLiveData avec l'index du dé, sa valeur
+//              3- Dans un ViewModelScope créer la coroutine qui va calculer la valeur du dé
+//                  et qui va permettre pendant deux secondes de calculer 20 valeurs avant que le dé ne se stabilise
+//              4- Dans la MainActivity créer un Observer qui va permettre d'afficher les dés
 
 class MainActivity : AppCompatActivity() {
 
