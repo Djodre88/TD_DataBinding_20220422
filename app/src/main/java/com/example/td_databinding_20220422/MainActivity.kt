a3c4b154407514e8cb604cac999f11308a4b5d76
djodre <jodre.diangienda@gmail.com>
2022-04-22 10:08:00 +0200

    création person1

--- app/src/main/java/com/example/td_databinding_20220422/MainActivity.kt
+++ app/src/main/java/com/example/td_databinding_20220422/MainActivity.kt
@@ -2,10 +2,22 @@ package com.example.td_databinding_20220422
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.example.td_databinding_20220422.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val binding : ActivityMainBinding = DataBindingUtil.setContentView(
+                    this,
+                    R.layout.activity_main)
+
+        val person1 = Person(
+            1,
+            "Doe",
+            "John",
+            30)
+
+        binding.person = person1
     }
 }
